2dfca9953842245d8271910d8d4b97e66fd98123
Richard <rdesilvey@gmail.com>
2021-04-19 20:17:12 -0600

    Refactor repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,91 +1,56 @@
 require 'bigdecimal'
+require_relative './repository'
 
-class ItemRepository
-  attr_reader :items
-
+class ItemRepository < Repository
   def inspect
-    "#<#{self.class} #{@items.size} rows>"
+    "#<#{self.class} #{@models.size} rows>"
   end
 
   def initialize(filename)
-    @items = create_items(filename)
-  end
-
-  def create_items(filename)
-    FileIo.process_csv(filename, Item)
-  end
-
-  def all
-    @items
-  end
-
-  def find_by_id(id)
-    @items.find { |item| item.id == id }
+    load_models(filename, Item)
   end
 
   def find_by_name(name)
-    @items.find do |item|
+    find_by do |item|
       item.name.casecmp?(name)
     end
   end
 
   def find_all_with_description(description)
-    @items.find_all do |item|
+    find_all_by do |item|
       item_description = item.description.downcase
       item_description.include?(description.downcase)
     end
   end
 
   def find_all_by_price(price)
-    @items.find_all do |item|
+    find_all_by do |item|
       item.unit_price == price
     end
   end
 
   def find_all_by_price_in_range(range)
-    @items.find_all do |item|
+    find_all_by do |item|
       range.cover?(item.unit_price)
     end
   end
 
   def find_all_by_merchant_id(merchant_id)
-    @items.find_all do |item|
+    find_all_by do |item|
       item.merchant_id == merchant_id
     end
   end
 
-  def create_first_item(attributes)
-    item = Item.new(attributes)
-    item.update_id(1)
-    @items << item
-  end
-
-  def find_max_id
-    @items.max_by(&:id).id
-  end
-
   def create(attributes)
-    if @items == []
-      create_first_item(attributes)
-    else
-      new_item = Item.new(attributes)
-      new_item.update_id(find_max_id + 1)
-      @items << new_item
-    end
+    item = Item.new(attributes)
+    super(item)
   end
 
   def update(id, attributes)
-    item = find_by_id(id)
-    unless item.nil?
+    super(id, attributes) do |item|
       item.update_name(attributes[:name])
       item.update_description(attributes[:description])
       item.update_unit_price(attributes[:unit_price])
-      item.update_time
     end
-  end
-
-  def delete(id)
-    item = find_by_id(id)
-    @items.delete(item)
   end
 end
