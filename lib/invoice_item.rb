d87306d7694a89a506eedb9e547a2ee93e9b7875
sami-p <sami.peterson14@gmail.com>
2021-04-20 12:30:04 -0500

    Refactor InvoiceItem for inheritance

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,28 +1,21 @@
-class InvoiceItem
-  attr_reader :id,
-              :item_id,
+require './lib/model'
+
+class InvoiceItem < Model
+  attr_reader :item_id,
               :invoice_id,
               :quantity,
               :unit_price
 
   def initialize(details)
-    @id = details[:id].to_i
+    super
     @item_id = details[:item_id].to_i
     @invoice_id = details[:invoice_id].to_i
     @quantity = details[:quantity]
     @unit_price = BigDecimal(details[:unit_price]) / 100
-    @created_at = details[:created_at]
-    @updated_at = details[:updated_at]
   end
 
   def unit_price_to_dollars
     @unit_price.to_f
-  end
-
-  def update_id(id)
-    return false if id.nil?
-
-    @id = id
   end
 
   def update_quantity(quantity)
@@ -31,21 +24,5 @@ class InvoiceItem
 
   def update_unit_price(unit_price)
     @unit_price = unit_price unless unit_price.nil?
-  end
-
-  def new_updated_at_time
-    @updated_at = Time.now
-  end
-
-  def created_at
-    return @created_at if @created_at.instance_of?(Time)
-
-    Time.parse(@created_at)
-  end
-
-  def updated_at
-    return @updated_at if @updated_at.instance_of?(Time)
-
-    Time.parse(@updated_at)
   end
 end
