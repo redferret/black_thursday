83c2b7aec5d1bfd02b4a525fa2edb975c9c2ee7d
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-19 18:17:08 -0600

    Create test for negative result

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -250,7 +250,17 @@ describe TransactionRepository do
       first_transaction = t_repo.transactions.first
       allow(first_transaction).to receive(:success?) { true }
 
-      expect(t_repo.any_success?(8))
+      expect(t_repo.any_success?(8)).to eq true
+    end
+
+    it 'returns false if no successful Transaction exists for invoice_id' do
+      mock_hashes = TransactionMocks.transactions_as_hashes(number_of_hashes: 4, invoice_id: 8, result: :success)
+      mock_data = TransactionMocks.transactions_as_mocks(self, mock_hashes)
+      allow_any_instance_of(TransactionRepository).to receive(:create_transactions).and_return(mock_data)
+      t_repo = TransactionRepository.new('fake.csv')
+      allow_any_instance_of(RSpec::Mocks::InstanceVerifyingDouble).to receive(:success?) { false }
+
+      expect(t_repo.any_success?(8)).to eq false
     end
   end
 end
